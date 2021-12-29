f8ad443bc939dace88fb00161ede12b292ca1ab9
bastien.vigneron <bastien.vigneron@outscale.com>
2021-12-29 13:53:09 +0100

    wip

---
+++ routes/national_report_2008.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"html/template"
+	"net/http"
+
+	"git.solutions.im/XeroxAgriCensus/AgriTracking/templates"
+	"github.com/gin-gonic/gin"
+)
+
+/*
+Only the booklet number is unique
+*/
+func (srv *Server) national_report_2008(footer string) {
+	srv.router.GET("/production/national_report_2008.html", func(c *gin.Context) {
+		header, _ := templates.RenderHeader(c)
+		data := gin.H{
+			"Header": template.HTML(header),
+			"Footer": template.HTML(footer),
+		}
+		c.HTML(http.StatusOK, "national_report_2008.html", data)
+	})
+}
