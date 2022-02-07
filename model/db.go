50c092fb56f66f6cf799b0652059ada787c698f2
bastien.vigneron <bastien.vigneron@outscale.com>
2022-02-07 19:03:07 +0100

    update table 33

--- model/db.go
+++ model/db.go
@@ -231,8 +231,11 @@ func (db *Db) GetAgregate(division, district, upazilla, union, mouza, tableName 
 	case "32":
 		columns = "sum(c29h+c29f)"
 		conditions = "c18 >= 0.05 GROUP BY rmo"
+	case "33":
+		columns = "sum(t101+t102+t103+t104+t105+t112+t113+t114+t121+t122+t123+t124+t125+t127+t128+t129+t130+t131+t132+t134+t135+t157+t158+t159+t160+t161+t167+t169+t175+t176+t177+t179+t182+t185+t106+t107+t108+t109+t110+t111+t115+t116+t117+t118+t119+t120+t126+t133+t136+t137+t138+t139+t140+t141+t142+t143+t144+t145+t146+t147+t148+t149+t150+t151+t152+t153+t154+t155+t156+t162+t163+t164+t165+t166+t168+t170+t171+t172+t173+t174+t178+t180+t181+t183+t184+t186+t187+t188+t189+t190+t191+t192+t193+t194+t195+t196+t197+t198+t199+t200+t201+t202+t203)/c13"
+		conditions = "true = true GROUP BY rmo, c13"
 	case "34":
-		columns = "sum(code13)"
+		columns = "sum(c13)"
 
 	default:
 		return tableData, fmt.Errorf(("don't know this table name"))
