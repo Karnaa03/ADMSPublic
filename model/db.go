adac4e5addc6d40651cd16a38fd5492e19162786
Bernard Beck <bbeck@bbk-consulting.com>
2022-04-29 18:59:07 +0000

    Revised ticket 66 on indicator 11

--- model/db.go
+++ model/db.go
@@ -179,13 +179,11 @@ func (db *Db) GetAgregate(division, district, upazilla, union, mouza, tableName 
 		columns = "sum(hh_f)"
 		conditions = "hh_f = 1 GROUP BY rmo"
 	case "9":
-		columns = "c02m+c02f+c02h+c03m+c03f+c03h"
+		columns = "SUM(+c02f+c0c02m2h+c03m+c03f+c03h)/SUM hh_sno"
 	case "10":
-		columns = "c02m+c02f+c02h+c03m+c03f+c03h "
-		conditions = "c18 >= 0.05"
+		columns = "SUM (c02mfarm+c02ffarm+c02hfarm+c03mfarm+c03ffarm+c03hfarm) / SUM (sf+mf+lf) "
 	case "11":
-		columns = "sum(c07)"
-		conditions = "true = true GROUP BY rmo"
+		columns = "sum(c07) / SUM (hh_sno)"
 	case "12":
 		columns = "sum(c07)"
 		conditions = "c18 >= 0.05 GROUP BY rmo"
