97920d5c7b177fdaec0c8081e6bb229e800d30e5
Bernard Beck <bbeck@bbk-consulting.com>
2022-05-06 12:37:03 +0000

    WIP

--- routes/household_poultry_information.go
+++ routes/household_poultry_information.go
@@ -22,23 +22,19 @@ func (srv *Server) FormatHouseholdPoultryInformation(division, district, upazill
 			<td>%s</td>
 			<td>%s</td>
 			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
 		</tr>
 								
 		`,
 			hli.Name,
 			p.Sprintf("%d", hli.NumberOfReportingHoldings),
 			p.Sprintf("%d", hli.TotalNumberOfPoultry),
-			p.Sprintf("%d", hli.NumberOfHouseholdPoultry),
-			p.Sprintf("%d", hli.NumberOfHouseholdAttachFarmPoultry),
 			p.Sprintf("%.2f", (float64(hli.TotalNumberOfPoultry)/float64(hli.NumberOfReportingHoldings))),
 		)
 	}
 
 	tableAndDonut = fmt.Sprintf(`
 	<div class="x_content">
-	<h4>Result<small> ফলাফল</small></h4>
+	<h4>Result</h4>
 	<h5>Data for table number : %s</h5>
 	<table id="datatable-buttons" class="table table-striped">
 		<thead>
@@ -46,8 +42,6 @@ func (srv *Server) FormatHouseholdPoultryInformation(division, district, upazill
 				<th>Report</th>
 				<th>Number of reporting holdings</th>
 				<th>Total number of poultry</th>
-				<th>Number of household poultry</th>
-				<th>Number of household attach farm poultry</th>
 				<th>Average type of poultry per holding</th>
 			</tr>
 		</thead>
